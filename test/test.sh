76fec7e773541461404011d1835afcaed386a72d
Arthur Chaloin <arthur@achaloin.com>
2019-04-02 18:24:19 +0200

    feat(ci): add test script

---
+++ test/test.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+function assert {
+  if [ $1 -ne $2 ]
+  then
+    echo "assertion failed: $1 == $2"
+    exit 1
+  fi
+}
+
+function test {
+  echo "testing: $1"
+  ./validator $1
+  assert $? $2
+}
+
+test test/valid.atom 0
+test test/invalid.atom 1
+test test/tooshort.atom 1
